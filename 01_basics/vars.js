c38990ed6099792a8adec921cbf1e28f2072e176
RidhimaS123 <125490703+RidhimaS123@users.noreply.github.com>
2024-06-09 11:25:55 +0000

    uptodatatypes

---
+++ 01_basics/vars.js
@@ -0,0 +1,10 @@
+const accountId=123354
+let accountEmail="ridhima@google.com"
+var accountPassword="12345"
+let accountCity="Jaipur"
+accountEmail="sharma22345@google.com"
+accountPassword="65378"
+accountCity="Bengaluru" 
+let accountState;
+// accountId=2
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
