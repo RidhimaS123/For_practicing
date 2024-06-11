9bc0ca7944e66388b26e8fc512d4014fbddeb913
RidhimaS123 <125490703+RidhimaS123@users.noreply.github.com>
2024-06-11 06:27:43 +0000

    uptoobjects

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,27 @@
+// const name="ridhima"
+// const repocount=50
+// console.log(`My name is ${name.toUpperCase()} and my repocount is ${repocount}`);
+// //used for string concatenation.
+// let mydate=new Date()
+// console.log(mydate.toString());
+// console.log(mydate.getTimezoneOffset())
+// console.log(mydate.toDateString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toLocaleString())
+// console.log(mydate.toTimeString())
+// console.log(mydate.toUTCString())
+// console.log(typeof(mydate))
+// const myarray=[0,1,2,3,4,true,"ridhima"]
+// console.log(myarray);
+const obj1={1:"a",2:"b"}
+const tinderUser={}
+tinderUser.id='123abc'
+tinderUser.email="ridhima@google.com"
+tinderUser.isloggedin=false
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty('isloggedin'));
